d04b46f5ab7861200960767c38bcfc9160cd501b
IlyaBurch <ilyaburch@yandex.ru>
2024-01-28 22:42:51 +0300

    Update app.vue

--- app.vue
+++ app.vue
@@ -9,4 +9,10 @@
 <script>
 // import { HeaderMenu } from '#build/components';
 import 'primevue/resources/themes/aura-light-green/theme.css'
+
+useHead({
+  meta: [
+    {name:"yandex-verification", content:"9c9f6db753017c6a" }
+  ]
+})
 </script>
